d89457fc8bdba05d0f72a8e722c6afd92922fee5
Aditya239233 <aditya.chandrasekhar2001@gmail.com>
2020-10-22 17:49:14 +0800

    updated CourseManager

---
+++ src/controller/CourseManager.java
@@ -0,0 +1,36 @@
+package controller;
+
+import model.Course;
+import model.Index;
+import model.Session;
+
+import java.util.List;
+
+public class CourseManager {
+	private static List<Course> CourseList;
+	
+	public static void addCourse(String Name, String ID, String faculty) {
+		Course newCourse = new Course(Name, ID, faculty);
+		CourseList.add(newCourse);
+		System.out.println("Course Added");
+	}
+	
+	public static void deleteCourse(String CourseID) {
+		if (checkIfCourseExists(CourseID)) {
+			for (Course course : CourseList) {
+				if (CourseID.equals(course.getID())) {
+					CourseList.remove(course);
+					break;
+				}
+			}
+			System.out.println("Course Has been deleted");
+		}
+		else {
+			System.out.println("Course Does not Exist");
+		}
+	}
+	
+	public static boolean checkIfCourseExists(String CourseID) {
+		return CourseList.stream().anyMatch(Course -> CourseID.equals(Course.getID()));
+	}
+}
