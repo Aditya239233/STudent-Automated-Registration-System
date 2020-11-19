29099720bbcd306722c26a9be84dfa7edfe9ae9d
perorin2 <72805115+perorin2@users.noreply.github.com>
2020-11-19 16:57:05 +0800

    added checkIndexSlot
    added printStudentListIndex, printStudentListCourse

--- src/view/AdminUI.java
+++ src/view/AdminUI.java
@@ -2,9 +2,15 @@ package view;
 
 import java.util.Scanner;
 
+import controller.CourseManager;
+import controller.IndexManager;
+import controller.StudentManager;
+import model.Index;
+
 public class AdminUI {
 
 	private int choice;
+	private IndexManager im = new IndexManager();
 	Scanner sc = new Scanner(System.in);
 
 	public void display() {
@@ -42,12 +48,15 @@ public class AdminUI {
 				break;
 			case 6:
 				// Check available slot for an index number
+				checkIndexSlot();
 				break;
 			case 7:
 				// Print student list by index number
+				printStudentListIndex();
 				break;
 			case 8:
 				// Print student list by course
+				printStudentListCourse();
 				break;
 			case 9:
 				break;
@@ -56,5 +65,50 @@ public class AdminUI {
 			}
 
 		} while (choice != 7);
+	}
+	
+	
+	
+	public void checkIndexSlot() {
+		System.out.println("Enter the Index: ");
+		String indexID = sc.next();
+		while(!IndexManager.checkIfIndexExists(indexID)) {
+			System.out.println("Invalid Index, please enter a valid Index: ");
+			indexID = sc.next();
+		}
+		for (int i = 0; i < IndexManager.IndexList.size(); i++) {
+			if (indexID == IndexManager.IndexList.get(i)) {
+				System.out.println(indexID + "has" + IndexManager.IndexList.get(i).getTotalVacancies() + "vacancies");
+			}
+		}
+		
+	}
+	
+	public void printStudentListIndex() {
+		System.out.println("Enter the Index: ");
+		String indexID = sc.next();
+		while(!IndexManager.checkIfIndexExists(indexID)) {
+			System.out.println("Invalid Index, please enter a valid Index: ");
+			indexID = sc.next();
+		}
+		for (int i = 0; i < StudentManager.StudentList.size(); i++) {
+			if (indexID == StudentManager.StudentList.get(i).getIndexes()) {
+				System.out.println(StudentManager.StudentList.get(i));
+			}
+		}
+	}
+	
+	public void printStudentListCourse() {
+		System.out.println("Enter the Couse: ");
+		String course = sc.next();
+		while(!CourseManager.checkIfCourseExists(course)){
+			System.out.println("Invalid course code, please enter a valid course code: ");
+			course = sc.next();
+		}
+		for (int i = 0; i < StudentManager.StudentList.size(); i++) {
+			if (course == StudentManager.StudentList.get(i).getID()) {
+				System.out.println(StudentManager.StudentList.get(i));
+			}
+		}
 	}
 }
